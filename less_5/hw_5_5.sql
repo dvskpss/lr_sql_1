18ca4a6e90b60a311f1321ac4ac85bbf0253cc4a
dvs <x@yy.zzz>
2021-02-14 17:45:04 +0300

    HW 5 OK

---
+++ less_5/hw_5_5.sql
@@ -0,0 +1,7 @@
+/*(по желанию) Из таблицы catalogs извлекаются записи при помощи запроса. 
+SELECT * FROM catalogs WHERE id IN (5, 1, 2); 
+Отсортируйте записи в порядке, заданном в списке IN.*/
+
+select * from catalogs;
+
+SELECT * FROM catalogs WHERE id IN (5, 1, 2) ORDER BY FIELD(id, 5, 1, 2);
